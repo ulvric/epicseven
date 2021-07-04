5cc34cfa6ac30cac4df7c7a441f4e3fb82031cd1
Jason Xiong <jpanda109@users.noreply.github.com>
2021-07-04 12:59:07 -0700

    clear arti

--- src/components/SelectArtifact.vue
+++ src/components/SelectArtifact.vue
@@ -1,78 +1,34 @@
 <template>
-  <select
-    :value="value"
-    class="selectpicker"
-    :title="title"
-    data-live-search="true"
-    data-width="100%"
-    @change="select($event)"
-  >
-    <option v-for="item in artifacts" :key="item._id" :data-tokens="getTokens(item)">
-      {{ item.name }}
-    </option>
-  </select>
+  <div>
+    <v-select
+      data-width="100%"
+      v-model="selected"
+      :value="selected"
+      label="name"
+      :options="artifacts"
+      @input="setSelected"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import $ from 'jquery';
+import vSelect from 'vue-select';
 import 'bootstrap-select';
 import { en } from '../assets/js/en.artifacts';
-import { fr } from '../assets/js/fr.artifacts';
-import { cn } from '../assets/js/cn.artifacts';
-import { tw } from '../assets/js/tw.artifacts';
-import { nicknames } from '../assets/js/nicknames';
+import 'vue-select/dist/vue-select.css';
 
 export default Vue.extend({
-  props: ['title', 'value'],
+  components: { vSelect },
   data() {
     return {
+      selected: null,
       artifacts: en,
-      nicknames,
     };
   },
-  created() {
-    this.artifacts = this.getItems();
-  },
-  watch: {
-    title(): void {
-      this.artifacts = this.getItems();
-    },
-  },
-  updated() {
-    $(this.$el).selectpicker({ title: this.title }).selectpicker('render');
-  },
   methods: {
-    select($event: Event): void {
-      this.$emit('input', ($event.target as HTMLInputElement).value);
-    },
-    /* eslint-disable  @typescript-eslint/no-explicit-any */
-    getItems(): Array<any> {
-      let artifacts = null;
-      switch (this.$i18n.locale) {
-        case 'en':
-          artifacts = en;
-          break;
-        case 'fr':
-          artifacts = fr;
-          break;
-        case 'cn':
-          artifacts = cn;
-          break;
-        case 'tw':
-          artifacts = tw;
-          break;
-        default:
-          artifacts = en;
-          break;
-      }
-      return artifacts;
-    },
-    getTokens(item: { _id: string }): string {
-      if (item._id && nicknames[item._id]) {
-        return nicknames[item._id];
-      }
-      return '';
+    setSelected(value: { _id: string; name: string }): void {
+      this.$emit('input', value.name);
     },
   },
 });
